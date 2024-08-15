d84ffbec101fd4a1bcd93cb14302745cb8bcb9a3
breduarm <bryan.e.armijos@gmail.com>
2024-08-15 11:00:00 -0500

    deps(mockk): Add kotlinx coroutines test dependencies

--- app/src/test/java/com/beam/tictactoexml/data/BoardRepositoryTest.kt
+++ app/src/test/java/com/beam/tictactoexml/data/BoardRepositoryTest.kt
@@ -1,13 +1,30 @@
 package com.beam.tictactoexml.data
 
 import com.beam.tictactoexml.data.datasource.BoardLocalDataSource
+import com.beam.tictactoexml.domain.TicTacToe
+import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class BoardRepositoryTest {
 
+    private val expectedBoard = TicTacToe()
+
     @Test
-    fun `when board is called, then return board from remote data source`() {
-        val localDataSource: BoardLocalDataSource = mockk()
+    fun `when board is called, then return board from remote data source`() = runTest {
+        val localDataSource: BoardLocalDataSource = mockk {
+            every { board } returns flowOf(expectedBoard)
+        }
+        val boardRepository = BoardRepository(localDataSource)
+
+        val actualBoard: TicTacToe = boardRepository.board.first()
+
+        assertEquals(expectedBoard, actualBoard)
     }
 }
