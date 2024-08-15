e1e9d2201099f44baf9a35619de30a0a31fe6565
breduarm <bryan.e.armijos@gmail.com>
2024-08-15 12:00:00 -0500

    test: Add tests for BoardRepository

--- app/src/test/java/com/beam/tictactoexml/data/BoardRepositoryTest.kt
+++ app/src/test/java/com/beam/tictactoexml/data/BoardRepositoryTest.kt
@@ -2,6 +2,8 @@ package com.beam.tictactoexml.data
 
 import com.beam.tictactoexml.data.datasource.BoardLocalDataSource
 import com.beam.tictactoexml.domain.TicTacToe
+import io.mockk.coJustRun
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -26,5 +28,31 @@ class BoardRepositoryTest {
         val actualBoard: TicTacToe = boardRepository.board.first()
 
         assertEquals(expectedBoard, actualBoard)
+    }
+
+    @Test
+    fun `When move is called, then save move in local data source`() = runTest {
+        val localDataSource: BoardLocalDataSource = mockk {
+            every { board } returns flowOf(expectedBoard)
+            coJustRun { saveMove(any(), any()) }
+        }
+        val boardRepository = BoardRepository(localDataSource)
+
+        boardRepository.move(0, 0)
+
+        coVerify { localDataSource.saveMove(0, 0) }
+    }
+
+    @Test
+    fun `When reset is called, then reset board in local data source`() = runTest {
+        val localDataSource: BoardLocalDataSource = mockk {
+            every { board } returns flowOf(expectedBoard)
+            coJustRun { reset() }
+        }
+        val boardRepository = BoardRepository(localDataSource)
+
+        boardRepository.reset()
+
+        coVerify { localDataSource.reset() }
     }
 }
