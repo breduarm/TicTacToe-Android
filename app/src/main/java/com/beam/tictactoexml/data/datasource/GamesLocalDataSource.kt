9fbdb0f3d185eceb5b76b70982cb21318e4575d2
breduarm <bryan.e.armijos@gmail.com>
2024-08-16 13:00:00 -0500

    fix: Convert GameDatSource in a Singleton comp and hardcoded a list of game

--- app/src/main/java/com/beam/tictactoexml/data/datasource/GamesLocalDataSource.kt
+++ app/src/main/java/com/beam/tictactoexml/data/datasource/GamesLocalDataSource.kt
@@ -4,15 +4,24 @@ import com.beam.tictactoexml.domain.Game
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
+import javax.inject.Singleton
 
 interface GamesLocalDataSource {
     val games: Flow<List<Game>>
     suspend fun addGame(game: Game)
 }
 
+@Singleton
 class GamesDataSource @Inject constructor() : GamesLocalDataSource {
+    private val mockGames = listOf(
+        Game("Game 1"),
+        Game("Game 2"),
+        Game("Game 3"),
+        Game("Game 4"),
+        Game("Game 5"),
+    )
 
-    private val _games: MutableStateFlow<List<Game>> = MutableStateFlow(emptyList())
+    private val _games: MutableStateFlow<List<Game>> = MutableStateFlow(mockGames)
     override val games: Flow<List<Game>> = _games
 
     override suspend fun addGame(game: Game) {
