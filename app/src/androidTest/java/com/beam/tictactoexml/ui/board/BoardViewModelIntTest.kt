bcf2962a850e574cdb34b2337c03b0945b5743d8
breduarm <bryan.e.armijos@gmail.com>
2024-09-04 14:00:00 -0500

    test(coroutines): Update GamesViewModelIntTest to use Coroutines rule to avoid use turbine

--- app/src/androidTest/java/com/beam/tictactoexml/ui/board/BoardViewModelIntTest.kt
+++ app/src/androidTest/java/com/beam/tictactoexml/ui/board/BoardViewModelIntTest.kt
@@ -1,15 +1,17 @@
 package com.beam.tictactoexml.ui.board
 
-import app.cash.turbine.test
 import com.beam.tictactoexml.data.remote.MockWebServerRule
 import com.beam.tictactoexml.domain.GameState
 import com.beam.tictactoexml.domain.TicTacToe
 import com.beam.tictactoexml.domain.move
+import com.beam.tictactoexml.testrules.CoroutinesTestRule
 import com.beam.tictactoexml.usecases.GetCurrentBoardUseCase
 import com.beam.tictactoexml.usecases.MakeBoardMoveUseCase
 import com.beam.tictactoexml.usecases.ResetBoardUseCase
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -17,14 +19,18 @@ import org.junit.Rule
 import org.junit.Test
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltAndroidTest
 class BoardViewModelIntTest {
 
-    @get:Rule
+    @get:Rule(order = 0)
     val hiltRule = HiltAndroidRule(this)
 
-    @get:Rule
+    @get:Rule(order = 1)
     val mockWebServerRule = MockWebServerRule()
+
+    @get:Rule
+    val coroutinesTestRule = CoroutinesTestRule()
 
     @Inject
     lateinit var getCurrentBoardUseCase: GetCurrentBoardUseCase
@@ -53,49 +59,39 @@ class BoardViewModelIntTest {
     }
 
     @Test
-    fun when_game_is_started_then_game_is_in_progress() = runTest {
-        viewModel.state.test {
-            assertEquals(BoardViewModel.UiState(), awaitItem())
+    fun when_game_is_started_then_game_is_in_progress() {
+        val expectedState = BoardViewModel.UiState(gameState = GameState.InProgress)
 
-            viewModel.startGame()
+        viewModel.startGame()
 
-            assertEquals(BoardViewModel.UiState(gameState = GameState.InProgress), awaitItem())
-        }
+        assertEquals(expectedState, viewModel.state.value)
     }
 
     @Test
     fun when_a_move_is_made_then_game_state_changes() = runTest {
-        viewModel.state.test {
-            assertEquals(BoardViewModel.UiState(), awaitItem())
-            viewModel.startGame()
-            assertEquals(BoardViewModel.UiState(gameState = GameState.InProgress), awaitItem())
+        val expectedState = BoardViewModel.UiState(
+            ticTacToe = TicTacToe().move(0, 0),
+            gameState = GameState.InProgress
+        )
+        viewModel.startGame()
 
-            viewModel.move(0, 0)
-            val expectedState = BoardViewModel.UiState(
-                ticTacToe = TicTacToe().move(0, 0),
-                gameState = GameState.InProgress
-            )
+        viewModel.move(0, 0)
+        runCurrent()
+        val actualState: BoardViewModel.UiState = viewModel.state.value
 
-            assertEquals(expectedState, awaitItem())
-        }
+        assertEquals(expectedState, actualState)
     }
 
     @Test
     fun when_reset_then_game_is_cleared_and_in_progress() = runTest {
-        viewModel.state.test {
-            assertEquals(BoardViewModel.UiState(), awaitItem())
-            viewModel.startGame()
-            assertEquals(BoardViewModel.UiState(gameState = GameState.InProgress), awaitItem())
-            viewModel.move(0, 0)
-            val updatedState = BoardViewModel.UiState(
-                ticTacToe = TicTacToe().move(0, 0),
-                gameState = GameState.InProgress
-            )
-            assertEquals(updatedState, awaitItem())
+        val expectedState = BoardViewModel.UiState(gameState = GameState.InProgress)
+        viewModel.startGame()
+        viewModel.move(0, 0)
 
-            viewModel.resetGame()
+        viewModel.resetGame()
+        runCurrent()
+        val actualState: BoardViewModel.UiState = viewModel.state.value
 
-            assertEquals(BoardViewModel.UiState(gameState = GameState.InProgress), awaitItem())
-        }
+        assertEquals(expectedState, actualState)
     }
 }
