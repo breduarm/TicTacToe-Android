4df77e2b62b2c3c89544b2777f871d31ed704506
breduarm <bryan.e.armijos@gmail.com>
2024-09-06 12:00:00 -0500

    test: Update BoardUiTest to add initial cases for board ui

--- app/src/androidTest/java/com/beam/tictactoexml/ui/board/BoardUiTest.kt
+++ app/src/androidTest/java/com/beam/tictactoexml/ui/board/BoardUiTest.kt
@@ -1,9 +1,11 @@
 package com.beam.tictactoexml.ui.board
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import com.beam.tictactoexml.R
 import com.beam.tictactoexml.ui.InstrumentedTest
@@ -19,7 +21,29 @@ class BoardUiTest : InstrumentedTest() {
     val activityRule = activityScenarioRule<MainActivity>()
 
     @Test
-    fun test_espresso_works() {
-        onView(withId(R.id.start_btn)).check(matches(isDisplayed()))
+    fun when_start_clicked_then_game_board_is_displayed() {
+
+        onView(withId(R.id.start_btn)).perform(click())
+
+        onView(withId(R.id.board_view)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun when_first_cell_is_clicked_then_it_displays_X() {
+        onView(withId(R.id.start_btn)).perform(click())
+
+        onView(withId(R.id.btn_0_0)).perform(click())
+
+        onView(withId(R.id.btn_0_0)).check(matches(withText("X")))
+    }
+
+    @Test
+    fun when_second_cell_is_clicked_then_it_displays_O() {
+        onView(withId(R.id.start_btn)).perform(click())
+        onView(withId(R.id.btn_0_0)).perform(click())
+
+        onView(withId(R.id.btn_0_1)).perform(click())
+
+        onView(withId(R.id.btn_0_1)).check(matches(withText("O")))
     }
 }
