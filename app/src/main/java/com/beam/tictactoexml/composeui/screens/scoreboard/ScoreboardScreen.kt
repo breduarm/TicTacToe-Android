9fcd5010ad34d0d8f5640f3f7e73593f79b03a9a
breduarm <bryan.e.armijos@gmail.com>
2024-09-11 00:35:22 -0500

    fix: Show scores in Scoreboard screen correctly

--- app/src/main/java/com/beam/tictactoexml/composeui/screens/scoreboard/ScoreboardScreen.kt
+++ app/src/main/java/com/beam/tictactoexml/composeui/screens/scoreboard/ScoreboardScreen.kt
@@ -1,8 +1,10 @@
 package com.beam.tictactoexml.composeui.screens.scoreboard
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -10,13 +12,16 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.beam.tictactoexml.R
 import com.beam.tictactoexml.domain.O
 import com.beam.tictactoexml.domain.Score
 import com.beam.tictactoexml.domain.X
@@ -26,13 +31,32 @@ import java.util.Date
 
 @Composable
 fun ScoreboardScreen(viewModel: ScoreboardViewModel = hiltViewModel()) {
-    val uiState: ScoreboardViewModel.UiState by viewModel.state.collectAsState()
+    val state: ScoreboardViewModel.UiState by viewModel.state.collectAsState()
 
-    ScoreboardContent(scores = uiState.scores)
+    LaunchedEffect(viewModel) {
+        viewModel.onUiReady()
+    }
+
+    ScoreboardContent(state)
 }
 
 @Composable
-fun ScoreboardContent(scores: List<Score>) {
+fun ScoreboardContent(state: ScoreboardViewModel.UiState) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (state.scores.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.empty_scoreboard),
+                style = MaterialTheme.typography.subtitle1,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        } else {
+            ScoreList(scores = state.scores)
+        }
+    }
+}
+
+@Composable
+fun ScoreList(scores: List<Score>) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         contentPadding = PaddingValues(16.dp),
@@ -65,6 +89,9 @@ fun ScoreboardScreenPreview() {
         Score(winner = X, numberOfMoves = 10, date = Date()),
         Score(winner = O, numberOfMoves = 8, date = Date()),
     )
+    val stateMock = ScoreboardViewModel.UiState(
+        scores = scoresMock,
+    )
 
-    ScoreboardContent(scoresMock)
+    ScoreboardContent(stateMock)
 }
